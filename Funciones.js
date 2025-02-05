c8c6d58507b8788747473cd9704757add426b1df
monche123 <monbara11cole@gmail.com>
2025-02-04 20:21:34 -0600

    Initial commit

---
+++ Funciones.js
@@ -0,0 +1,30 @@
+
+var a = document.getElementById("login-btn");    
+var b = document.getElementById("register-btn");
+var x = document.getElementById("login");    
+var y = document.getElementById("register");
+function login() {
+        x.style.left = "4px";
+        y.style.right = "-520px";
+        a.className += " white-btn";
+        b.className = "btn";
+        x.style.opacity = 1;
+        y.style.opacity = 0;
+}
+function register() {
+        x.style.left = "-510px";
+        y.style.right = "5px";
+        a.className = "btn";
+        b.className += " white-btn";
+        x.style.opacity = 0;
+        y.style.opacity = 1;
+}
+
+function menuFunction() {
+    var i = document.getElementById("navMenu");
+    if(i.className === "nav-menu") {
+        i.className += " responsive";
+    } else {
+        i.className = "nav-menu";
+    }
+   }
